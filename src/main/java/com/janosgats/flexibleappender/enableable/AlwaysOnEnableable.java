d944a27b86661c245fabc690b964d6b30e5e101a
János Gats <gats.janos@accretivetg.com>
2020-01-30 17:21:31 +0100

    AlwaysOnEnableable is always on.

--- src/main/java/com/janosgats/flexibleappender/enableable/AlwaysOnEnableable.java
+++ src/main/java/com/janosgats/flexibleappender/enableable/AlwaysOnEnableable.java
@@ -3,6 +3,6 @@ package com.janosgats.flexibleappender.enableable;
 public class AlwaysOnEnableable extends AbstractEnableable {
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
